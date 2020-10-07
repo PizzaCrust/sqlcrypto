e3e8852f2a98ffd8d9d20c66a03d6cc1863eb5b1
PizzaCrust <pizzacrustcoder@gmail.com>
2020-10-07 14:29:49 +0700

    .

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::time::Instant;
 
 fn main() -> sqlcrypto::Result<()> {
-    let bytes = std::fs::read("encrypted.db").unwrap();
+    let bytes = std::fs::read("encrypted.db").unwrap(); // 115 sec rust crypto hmac check
     let mut output: Vec<u8> = Vec::with_capacity(bytes.len());
     let instant = Instant::now();
     sqlcrypto::decrypt(bytes.as_slice(), b"9bf9c6ed9d537c399a6c4513e92ab24717e1a488381e3338593abd923fc8a13b", &mut output).unwrap();
