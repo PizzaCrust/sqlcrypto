5090fa170c4aa3a10c8c9aa172aa6738210b8ae4
PizzaCrust <pizzacrustcoder@gmail.com>
2020-10-08 14:53:52 +0700

    ultra fast encryption

--- src/error.rs
+++ src/error.rs
@@ -21,6 +21,5 @@ macro_rules! create_error {
 
 create_error! {
     std::io::Error => Io
-    crypto::symmetriccipher::SymmetricCipherError => Cipher
     ring::error::Unspecified => RingUnspecified
 }
